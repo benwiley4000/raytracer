87fe2d668eb28613de34b43f9b7288f685e268be
Ben Wiley <therealbenwiley@gmail.com>
2017-11-04 02:37:26 -0400

    Functions renamed with camelCase.

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,10 @@
 #include "objects/ObjModel.hpp"
 
 // run in separate thread
-void raytrace_scene();
+void raytraceScene();
 
 // run in main thread
-void wait_for_input();
+void waitForInput();
 
 std::thread t;
 std::mutex mut;
@@ -33,16 +33,16 @@ int main()
 		20.0f
 	);
 
-	t = std::thread(raytrace_scene);
+	t = std::thread(raytraceScene);
 	std::string trash; // input that won't get used
 	while (!done) {
 		std::getline(std::cin, trash); // wait for user to hit enter
-		wait_for_input();
+		waitForInput();
 	}
 	return 0;
 }
 
-void raytrace_scene()
+void raytraceScene()
 {
 	std::chrono::seconds trace_duration(1);
 	std::chrono::microseconds wait_duration(100);
@@ -51,14 +51,14 @@ void raytrace_scene()
 	while (!done) {
 		std::cout << "Ray tracing scene... (enter any input to pause)" << std::endl;
 		std::this_thread::sleep_for(trace_duration);
-		mut.unlock(); // possibly defer to wait_for_input
+		mut.unlock(); // possibly defer to waitForInput
 		// sleep for extremely small duration to give main thread time to register inputs
 		std::this_thread::sleep_for(wait_duration);
-		mut.lock(); // pick up when wait_for_input has given back control
+		mut.lock(); // pick up when waitForInput has given back control
 	}
 }
 
-void wait_for_input()
+void waitForInput()
 {
 	std::cout << "Pausing ray trace..." << std::endl;
 	mut.lock();
@@ -68,11 +68,11 @@ void wait_for_input()
 		done = true;
 		mut.unlock();
 		if (t.joinable()) {
-			// wait for raytrace_scene thread to finish
+			// wait for raytraceScene thread to finish
 			t.join();
 		}
 		return;
 	}
 	std::cout << "Resuming ray trace..." << std::endl;
-	mut.unlock(); // return control to raytrace_scene
+	mut.unlock(); // return control to raytraceScene
 }
