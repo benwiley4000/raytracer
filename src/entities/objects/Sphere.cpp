64fa3f39706a8c7cb4bbc90de239d830d502b052
Ben Wiley <therealbenwiley@gmail.com>
2017-11-09 00:48:38 -0500

    Temporary better ray intersection for spheres.

--- src/entities/objects/Sphere.cpp
+++ src/entities/objects/Sphere.cpp
@@ -25,6 +25,7 @@ bool Sphere::doesRayIntersect(
 	glm::vec3* const& normal
 ) const
 {
+	/*
 	// formula based on slides 5-6 at:
 	// http://poullis.org/courses/2017/Fall/COMP371/resources/Lecture%2016_%20Ray%20Tracing_Geometric%20Queries.pdf
 	double b = 2 * (
@@ -34,13 +35,28 @@ bool Sphere::doesRayIntersect(
 	              );
 	double c = pow(origin.x - this->position.x, 2) +
 	           pow(origin.y - this->position.y, 2) +
-	           pow(origin.z - this->position.z, 2) +
+	           pow(origin.z - this->position.z, 2) -
 	           pow(this->radius, 2);
 	double sqrt_discriminant = sqrt(pow(b, 2) - 4 * c);
 	// get whichever intersection t value is smallest (may be smaller than origin)
 	*t = std::min((-b + sqrt_discriminant) / 2, (-b - sqrt_discriminant) / 2);
 
-	*normal = (1 / this->radius) * (origin + direction * *t) - this->position;
 
-	return *t >= t_threshold;
+	*normal = glm::normalize((origin + direction * *t) - this->position);
+
+	return *t >= t_threshold && !std::isnan(*t);
+	*/
+	// temp solution from:
+	// https://www.scratchapixel.com/code.php?id=3&origin=/lessons/3d-basic-rendering/introduction-to-ray-tracing
+	glm::vec3 l = this->position - origin;
+	float tca = l.x * direction.x + l.y *direction.y + l.z * direction.z;
+	if (tca <= t_threshold) return false;
+	float d2 = l.x * l.x + l.y * l.y + l.z * l.z - tca * tca;
+	if (d2 > this->radius * this->radius) return false;
+	double thc = sqrt(this->radius * this->radius - d2);
+	*t = std::min(tca - thc, tca + thc);
+
+	*normal = glm::normalize((origin + direction * *t) - this->position);
+
+	return true;
 }
